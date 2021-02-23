e7ef14299f2b392e11ce391e071ccdda7f28a466
fabien <fabien.mathieu@nokia-bell-labs.com>
2021-02-23 19:05:04 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/balouf/sisu',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
     cmdclass={"develop": PostDevelop, "install": PostInstall},
 )
